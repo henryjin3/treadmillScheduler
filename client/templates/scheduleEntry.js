0a5c462550d570f9fbd9e0dd7a2f957402ba923b
Henry Jin <henry@entrancesoftware.com>
2015-10-02 11:04:17 -0500

    #1 Add a reserve button which integrates with current google user account.

--- client/templates/scheduleEntry.js
+++ client/templates/scheduleEntry.js
@@ -1,10 +1,14 @@
 Template.body.events({
   "change #name": function(event) {
     var name = event.target.value;
-    console.log(name);
 
     ScheduleEntries.update(this._id, {
       $set: { name: name }
+    });
+  },
+  "click #reserveBtn": function(event) {
+    ScheduleEntries.update(this._id, {
+      $set: { name: Meteor.user().profile.name }
     });
   }
 })
